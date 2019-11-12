1021a7c0be082f4db284868adfc781a45267d962
leobsousa9 <57633571+leobsousa9@users.noreply.github.com>
2019-11-12 17:17:51 -0300

    Add files via upload

---
+++ Manager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+namespace Com.Kawalisun.SimpleHostile
+{
+    public class Manager : MonoBehaviourPunCallbacks
+    {
+        public string player_prefab;
+        public Transform spawn_point;
+
+        private void Start()
+        {
+            Spawn();
+        }
+
+        private void Spawn()
+        {
+            PhotonNetwork.Instantiate(player_prefab, spawn_point.position,spawn_point.rotation);
+        }
+
+    }
+
+}
